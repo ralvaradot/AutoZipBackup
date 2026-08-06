b5eb5f47d1d27f301fb91c25741a71fabfb8f751
Roberto Alvarado <ralvaradot@hotmail.com>
2026-08-06 16:20:42 -0500

    feat: create initial project structure

--- README.md
+++
@@ -1,3 +0,0 @@
-# AutoZipBackup
-
-Entrega 1.1 del proyecto.
--- app.py
+++
@@ -1,9 +0,0 @@
-from models.app_config import AppConfig
-
-def main():
-    print("AutoZip Backup - Entrega 1.1")
-    cfg=AppConfig()
-    print(cfg)
-
-if __name__=="__main__":
-    main()
